78149e0a31def2ba0d58d2be57d58b3002b2f698
zyskiu <zyskiu@163.com>
2016-12-09 21:36:59 +0800

    date time 日期时间 first explore

---
+++ javase_le/src/线程/Te.java
@@ -0,0 +1,30 @@
+package 线程;
+
+
+public class Te {
+	
+	public static void main(String[] args) {
+		MyTh myTh = new MyTh();
+		Thread thread = new Thread(myTh);
+		thread.start();
+		try {
+			System.out.println("main线程尝试获取MyTh锁");
+			MyTh.class.wait();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
+class MyTh implements Runnable{
+
+	@Override
+	public void run() {
+		synchronized (MyTh.class) {
+			System.out.println(Thread.currentThread().getName() + "获得了MyTh的类对象锁并且不释放锁");
+			while(true);
+		}
+	}
+	
+}
