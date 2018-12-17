0eef82d12152f6086e88e75844badc1021cf4129
zyskiu <zyskiu@163.com>
2018-12-17 23:18:13 +0800

    fastjson

---
+++ src/序列化/fastjson/Demo.java
@@ -0,0 +1,57 @@
+package 序列化.fastjson;
+
+import com.alibaba.fastjson.JSON;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Isen
+ * @date 2018/12/17 23:04
+ * @since 1.0
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "张三");
+        T t = new T();
+        t.setA(12);
+        t.setIds(Arrays.asList(12, 18));
+//        t.setMap(map);
+
+        System.out.println(JSON.toJSONString(t));
+    }
+}
+
+class T implements Serializable {
+    private int a;
+    private List<Integer> ids;
+    private Map<String, String> map;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public List<Integer> getIds() {
+        return ids;
+    }
+
+    public void setIds(List<Integer> ids) {
+        this.ids = ids;
+    }
+
+    public Map<String, String> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, String> map) {
+        this.map = map;
+    }
+}
